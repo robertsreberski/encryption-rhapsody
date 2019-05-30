6969c38b8ac54e406e3a4079c2e52cd60139f287
robertsreberski <robert@straplegroup.com>
2019-05-31 01:18:38 +0200

    Implemented AES encryption on server

--- server/src/algorithms/encrypt.ts
+++ server/src/algorithms/encrypt.ts
@@ -1,5 +1,30 @@
 import * as crypto from 'crypto'
-import * as fs from 'fs'
+import ReadStream = NodeJS.ReadStream;
+import WriteStream = NodeJS.WriteStream;
 
-const baseAlgorithm = 'aes-192'
+const size = 128
+const baseAlgorithm = 'aes'
+
+export enum Mode { ECB = 'ecb', CBC = 'cbc', CFB = 'cfb', OFB = 'ofb' }
+
+const getAlgorithm = (size: number = 128, mode: Mode = Mode.ECB) => `${baseAlgorithm}-${size}-${mode}`
+
+const createKey = (bitLength: number = 128) => {
+  const byteLength = bitLength / 8
+  const secret = crypto.randomBytes(byteLength)
+  const salt = crypto.randomBytes(byteLength)
+  return crypto.scryptSync(secret, salt, byteLength)
+}
+
+const createIv = (mode: Mode) => {
+  return crypto.randomBytes(mode !== Mode.ECB ? 16 : 0)
+}
+
+export const encrypt = async (input: ReadStream, output: WriteStream, mode: Mode) => {
+  const key = createKey(size)
+  const iv = createIv(mode)
+  const cipher = crypto.createCipheriv(getAlgorithm(size, mode), key, iv)
+  input.pipe(cipher).pipe(output)
+}
+
 
--- server/src/index.tsx
+++ server/src/index.tsx
@@ -1,6 +1,21 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
+import { encrypt, Mode } from "./algorithms/encrypt";
+const { dialog } = require('electron').remote
 const fs = require('fs');
+
+dialog.showOpenDialog(async (paths: string) => {
+  const selected = paths ? paths[0] : null
+  if (null) return
+
+  for (let mode in Mode) {
+    console.log(mode)
+    const input = fs.createReadStream(selected)
+    const output = fs.createWriteStream(`${selected}.crypt.${Mode[mode]}`)
+    await encrypt(input, output, Mode[mode] as Mode)
+  }
+
+})
 
 ReactDOM.render(<div><h1>Welcome!</h1><ul>
   {fs.readdirSync('.').map((filename: string) => (
